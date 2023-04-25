d16b0427f8804ec01369eb7f876ab7a5d9d72d6c
Pavel Kirilin <win10@list.ru>
2023-04-25 14:49:24 +0400

    Refactored initializer + fixed types.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- aiohttp_deps/initializer.py
+++ aiohttp_deps/initializer.py
@@ -8,51 +8,86 @@ from taskiq_dependencies import DependencyGraph
 from aiohttp_deps.view import View
 
 
-def _function_handler(
-    original_route: Callable[..., Awaitable[web.StreamResponse]],
-) -> Callable[[web.Request], Awaitable[web.StreamResponse]]:
-    original_handler = copy.copy(original_route)
-    graph = DependencyGraph(original_handler)
+class InjectableFuncHandler:
+    """
+    Dependency injector for function handlers.
 
-    async def custom_responder(request: web.Request) -> web.StreamResponse:
-        async with graph.async_ctx(
+    This class creates a dependency injection context on startup
+    and then use it to server requests.
+
+
+    The `__call__` method is used to serve requests.
+
+    Usage:
+    new_handler = InjectableFuncHandler(old_handler)
+    response = await new_handler(request)
+    """
+
+    def __init__(
+        self,
+        original_route: Callable[..., Awaitable[web.StreamResponse]],
+    ) -> None:
+        self.original_handler = copy.copy(original_route)
+        self.graph = DependencyGraph(self.original_handler)
+
+    async def __call__(self, request: web.Request) -> web.StreamResponse:
+        """
+        Serve a request.
+
+        This function creates async context and resolves all kwargs,
+        required to serve current request.
+
+        :param request: current request.
+        :return: response.
+        """
+        async with self.graph.async_ctx(
             {
                 web.Request: request,
                 web.Application: request.app,
             },
-        ) as gra:
-            return await original_handler(**(await gra.resolve_kwargs()))
-
-    return custom_responder
+        ) as resolver:
+            return await self.original_handler(**(await resolver.resolve_kwargs()))
 
 
-def _view_handler(
-    original_route: Type[View],
-) -> Callable[[web.Request], Awaitable[web.StreamResponse]]:
-    allowed_methods = {
-        method.lower()
-        for method in hdrs.METH_ALL
-        if hasattr(original_route, method.lower())  # noqa: WPS421
-    }
-    graph_map = {
-        method: DependencyGraph(getattr(original_route, method))
-        for method in allowed_methods
-    }
+class InjectableViewHandler:
+    """
+    Dependency injector for views.
 
-    async def custom_responder(request: web.Request) -> web.StreamResponse:
-        return await original_route(request, graph_map)
+    This class is different from function injector,
+    because it calculates graphs for all implemented methods
+    of the view.
 
-    return custom_responder
+    Usage:
+    new_handler = InjectableViewHandler(MyView)
+    response = await new_handler(request)
+    """
 
+    def __init__(
+        self,
+        original_route: Type[View],
+    ) -> None:
+        self.original_handler = copy.copy(original_route)
+        allowed_methods = {
+            method.lower()
+            for method in hdrs.METH_ALL
+            if hasattr(original_route, method.lower())  # noqa: WPS421
+        }
+        self.graph_map = {
+            method: DependencyGraph(getattr(original_route, method))
+            for method in allowed_methods
+        }
 
-def _route_generator(
-    original_route: Callable[..., Awaitable[web.StreamResponse]],
-) -> Callable[[web.Request], Awaitable[web.StreamResponse]]:
-    if inspect.isclass(original_route):
-        if issubclass(original_route, View):
-            return _view_handler(original_route)
-        return original_route
-    return _function_handler(original_route)
+    async def __call__(self, request: web.Request) -> web.StreamResponse:
+        """
+        Serve a request.
+
+        This function creates async context and resolves all kwargs,
+        required to serve current request.
+
+        :param request: current request.
+        :return: response.
+        """
+        return await self.original_handler(request, self.graph_map)
 
 
 async def init(app: web.Application) -> None:
@@ -73,4 +108,8 @@ async def init(app: web.Application) -> None:
     :param app: current application.
     """
     for route in app.router.routes():
-        route._handler = _route_generator(route._handler)  # noqa: WPS437
+        if inspect.isclass(route._handler):
+            if issubclass(route._handler, View):
+                route._handler = InjectableViewHandler(route._handler)
+            continue
+        route._handler = InjectableFuncHandler(route._handler)
